2847b3a9dda629367287c88066a48681f9f75ec4
NjoguRufus <rsgxliv@gmail.com>
2024-08-15 08:50:00 +0300

    setup

---
+++ my-dashboard/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="text-center text-2xl mt-10">
+      Your Dashboard Will Be Here
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
